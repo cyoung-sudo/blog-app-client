83877ef4d35b8a2796ce917d309b750f622d5c9d
Christopher Young <christopheryoung@MacBook123.local>
2023-03-27 11:18:23 -0700

    added collapsed navbar menu

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,4 +1,6 @@
 import "./Navbar.css";
+// React
+import { useState } from "react";
 // Routing
 import { Link, NavLink, useNavigate } from "react-router-dom";
 // Redux
@@ -7,8 +9,12 @@ import { setAuthUser } from "../../appSlice";
 import { setPopup } from "../../components/popup/slices/popupSlice";
 // APIs
 import * as authAPI from "../../apis/authAPI";
+// Icons
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar({ authUser }) {
+  // Collapse toggle
+  const [collapse, setCollapse] = useState(true);
   // Hooks
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -36,7 +42,14 @@ export default function Navbar({ authUser }) {
       <div id="navbar-logo">
         <Link to="/">Blog App</Link>
       </div>
+
+      <div id="navbar-collapse-toggle">
+        <button onClick={() => setCollapse(state => !state)}>
+          <GiHamburgerMenu size={30}/>
+        </button>
+      </div>
       
+      {/*----- Expanded links -----*/}
       <ul id="navbar-links">
         <li>
           <NavLink
@@ -100,6 +113,75 @@ export default function Navbar({ authUser }) {
           </li>
         }
       </ul>
+      {/*----- /Expanded links -----*/}
+
+      {/*----- Collapsed links -----*/}
+      {!collapse &&
+        <ul id="navbar-collapsed-links">
+          <li>
+            <NavLink
+              to="users"
+              end
+              className={({ isActive }) =>
+                isActive ? "navbar-link-active" : undefined}>
+              Users
+            </NavLink>
+          </li>
+
+          {!authUser &&
+            <li>
+              <NavLink
+                to="signup"
+                className={({ isActive }) =>
+                  isActive ? "navbar-link-active" : undefined}>
+                Signup
+              </NavLink>
+            </li>
+          }
+
+          {!authUser &&
+            <li>
+              <NavLink
+                to="login"
+                className={({ isActive }) =>
+                  isActive ? "navbar-link-active" : undefined}>
+                Login
+              </NavLink>
+            </li>
+          }
+
+          {authUser &&
+            <li>
+              <NavLink
+                to={ `users/${authUser._id}` }
+                end
+                className={({ isActive }) =>
+                  isActive ? "navbar-link-active" : undefined}>
+                Profile
+              </NavLink>
+            </li>
+          }
+
+          {authUser &&
+            <li>
+              <NavLink
+                to="settings"
+                end
+                className={({ isActive }) =>
+                  isActive ? "navbar-link-active" : undefined}>
+                Settings
+              </NavLink>
+            </li>
+          }
+
+          {authUser &&
+            <li>
+              <button onClick={ handleLogout }>Logout</button>
+            </li>
+          }
+        </ul>
+      }
+      {/*----- /Collapsed links -----*/}
     </div>
   );
 };
